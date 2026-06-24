60ffac3be6ba3e59185fc11f35182b38aca54267
kanjyo-hirako <284790703@qq.com>
2026-06-24 20:40:28 +0800

    完善个人信息页面

--- code/travel_agent_fronted/src/views/Profile.vue
+++ code/travel_agent_fronted/src/views/Profile.vue
@@ -87,7 +87,7 @@
         />
         <van-cell
           title="版本信息"
-          value="v1.0.0"
+          value="v1.1.0"
         />
       </van-cell-group>
     </div>
@@ -127,7 +127,8 @@ const goHistory = () => {
 const showAbout = () => {
   showDialog({
     title: '关于我们',
-    message: '智能旅游助手 v1.1.0\n\n基于 AI 技术的智能旅游规划平台\n为您提供个性化的旅游行程推荐和实时旅游咨询服务\n\n© 2026 智能旅游助手',
+    allowHtml: true,
+    message: '智能旅游助手 v1.1.0<br><br>基于 AI 技术的智能旅游规划平台<br>为您提供个性化的旅游行程推荐<br>和实时旅游咨询服务<br><br>© 2026 智能旅游助手<br><br>更多信息请跳转到<br><a href="https://github.com/kanjyo-hirako/travel_agent" target="_blank" style="color:#1989fa;">https://github.com/kanjyo-hirako/travel_agent</a>',
     confirmButtonText: '知道了'
   }).catch(() => {})
 }
